004a6720e7e633429e5db17bea493375e082ce21
翔 玉河 <210306@jc-21.jp>
2023-07-13 12:29:44 +0900

    停止

--- Assets/NewBehaviourScript1.cs
+++ Assets/NewBehaviourScript1.cs
@@ -5,12 +5,12 @@ using DG.Tweening;
 
 public class NewBehaviourScript1 : MonoBehaviour
 {
-    private Rigidbody rigidbody;
+    //private Rigidbody rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
-        rigidbody.DOMove(new Vector3(0, 0, 50), 100);
+        //rigidbody = GetComponent<Rigidbody>();
+        //rigidbody.DOMove(new Vector3(0, 0, 50), 100);
     }
 
     // Update is called once per frame
